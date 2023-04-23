d668ddee92d9c87abc74e5c207be9bd4392656f0
Maxim <85497282+max9996661@users.noreply.github.com>
2023-04-23 22:13:22 +0300

    Основной код для задачи по 3 спринту

--- Counter Application/ViewController.swift
+++ Counter Application/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var valueCounter : Int = 0
+    var nameLabel : String = "Значение счётчика:"
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var counterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterLabel.adjustsFontSizeToFitWidth = true // Убираем ограничение на длинну строки
+        counterLabel.text = nameLabel + " " + String(valueCounter)
+        counterLabel.font = UIFont(name: "Times New Roman", size: 20)
+        
+        counterButton.tintColor = .black
+        counterButton.backgroundColor = .orange
+        counterButton.layer.cornerRadius = 6
     }
-
+    
+    
+ @IBAction func counterPlusOne(_ sender: UIButton) {
+        valueCounter += 1
+        counterLabel.text = nameLabel + " " +  String(valueCounter)
+    }
+    
 
 }
 
